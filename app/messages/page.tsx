7e2583a263957173a81e91689de3e7d8961e82ac
Aras M2 Pro <tvmonoton@gmail.com>
2025-03-21 10:54:10 +0300

    env

--- app/messages/page.tsx
+++ app/messages/page.tsx
@@ -61,7 +61,7 @@ export default function MessagesPage() {
             </div>
             
             <div className="flex-grow overflow-y-auto">
-              <MessageList 
+              <MessageList
                 chats={Object.values(chats)} 
                 selectedChatId={selectedChatId} 
                 onSelectChat={selectChat} 
